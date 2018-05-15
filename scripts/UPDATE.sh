56b75218df1dd82b8b435705ddd9db77af5f374d
Edoardo Pasca <edo.paskino@gmail.com>
2018-05-15 09:03:16 +0100

    removed creation of start_notebook script

--- scripts/UPDATE.sh
+++ scripts/UPDATE.sh
@@ -235,11 +235,6 @@ cp -vp $SIRF_SRC_PATH/CCPPETMR_VM/scripts/update*sh $SIRF_INSTALL_PATH/bin
 cd $SIRF_SRC_PATH
 clone_or_pull SIRF-Exercises
 
-echo "#! /bin/bash
-cd ${SIRF_SRC_PATH}/SIRF-Exercises/notebooks
-ipython notebook
-" > $SIRF_INSTALL_PATH/bin/start_notebook
-chmod +x $SIRF_INSTALL_PATH/bin/start_notebook
 # copy help file to Desktop
 if [ ! -d ~/Desktop ]
 then
