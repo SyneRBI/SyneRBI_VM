ef40eff37ab5487afd79b0ce8b94149e5a4c3308
Edoardo Pasca <edo.paskino@gmail.com>
2018-05-15 09:05:17 +0100

    removed file

--- scripts/update_VM_with_exercises.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-# script to install in the VM the packages needed for training.
-# Originally introduced for the PSMR2018 training event.
-
-echo "installing ipython notebook and browser"
-$SUDO apt-get install -y firefox python-pip
