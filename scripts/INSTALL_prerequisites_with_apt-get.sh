0bba4907a0b4f61783fe57e021f824964f59a92a
Edoardo Pasca <edo.paskino@gmail.com>
2019-03-04 11:29:50 +0000

    removed comment

--- scripts/INSTALL_prerequisites_with_apt-get.sh
+++ scripts/INSTALL_prerequisites_with_apt-get.sh
@@ -20,13 +20,8 @@ $SUDO apt-get install -y --no-install-recommends libhdf5-serial-dev git-core cma
       libarmadillo-dev libace-dev  \
       libgtest-dev \
       libopenblas-dev libatlas-base-dev \
-      libxml2-dev libxslt-dev \ # for siemens_to_ismrmrd
+      libxml2-dev libxslt-dev \ 
       cython
-#      g++-6 gcc-6 
-# Gadgetron dependencies, not required (yet) by SIRF
-# only plplot install once https://github.com/gadgetron/gadgetron/issues/608 fixed
-# in the Gadgetron version that we use
-# libplplot-dev
 
 echo "Installing boost 1.65 or later"
 # first find current boost version (if any)
