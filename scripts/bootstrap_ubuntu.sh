5d68ffee95cf017c1294591f4e97a959d0079ada
Edoardo Pasca <edo.pakino@gmail.com>
2021-04-19 13:47:26 +0100

    builds master

--- scripts/bootstrap_ubuntu.sh
+++ scripts/bootstrap_ubuntu.sh
@@ -64,7 +64,6 @@ cd $userHOME/devel
 if [ ! -d $userHOME/devel/SyneRBI_VM ]; then
   git clone https://github.com/SyneRBI/SyneRBI_VM.git
   cd SyneRBI_VM
-  git checkout python3_build
 else
   cd SyneRBI_VM
   git pull
